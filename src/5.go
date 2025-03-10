cdbdb02da8b84bd31d71ad50ff5c8ac72eb98baf
lanningeichert432 <lanningeichert432@gmail.com>
2025-03-10 09:56:26 +0000

    Add 5.go

---
+++ src/5.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declare variables
+	var x int = 4
+	var y int = 7
+
+	// Perform calculations
+	sum := x + y
+	diff := x - y
+	prod := x * y
+	quot := x / y
+
+	// Print results
+	fmt.Println("Sum:", sum)
+	fmt.Println("Difference:", diff)
+	fmt.Println("Product:", prod)
+	fmt.Println("Quotient:", quot)
+}
