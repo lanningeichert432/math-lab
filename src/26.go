7c8533b4e06ae4112a8b1769c3f56555514d27de
lanningeichert432 <lanningeichert432@gmail.com>
2025-04-03 13:35:11 +0000

    Add 26.go

---
+++ src/26.go
@@ -0,0 +1,6 @@
+public class GoCodeGenerator {
+    public static void main(String[] args) {
+        // Code to be executed in the Go environment
+        System.out.println("Hello, World!");
+    }
+}
