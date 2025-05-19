9d0ed311eaaeb88d0e8bda44923205e83c248756
lanningeichert432 <lanningeichert432@gmail.com>
2025-05-19 05:56:21 +0000

    Add 40.go

---
+++ src/40.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    var x float64 = 3.5
+    var y float64 = 2.0
+    if x == y {
+        fmt.Printf("x and y are equal\n")
+    } else {
+        fmt.Printf("x and y are not equal\n")
+    }
+}
