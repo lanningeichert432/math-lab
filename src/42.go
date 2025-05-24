ec0c6d5a6e59d7f2166bbf78c8d978847168a409
lanningeichert432 <lanningeichert432@gmail.com>
2025-05-24 13:55:40 +0000

    Add 42.go

---
+++ src/42.go
@@ -0,0 +1,8 @@
+public class Go {
+    public static void main(String[] args) {
+        int i = 5;
+        System.out.println("I am in line number " + i);
+        // Add more statements to demonstrate your Go code here
+        System.out.println("This is the end of the program.");
+    }
+}
